b2da2cc103b2a0ba341ea7ef7edbb4c0292e04ac
Walid Ahmed <walmaly@gmail.com>
2019-03-08 13:00:20 -0500

    Add files via upload

--- HandwritingRecognition.py
+++ HandwritingRecognition.py
@@ -1,9 +1,51 @@
 import tensorflow as tf
+import numpy as np
+
+print(tf.__version__)
+
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>=0.99):
+      print("Reached 99% accuracy so cancelling training!")
+      self.model.stop_training = True
+callbacks = myCallback()
+
 mnist = tf.keras.datasets.mnist
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
 import matplotlib.pyplot as plt
 plt.imshow(training_images[0])
-print(y_train[0])
-print(x_train[0])
+print(training_labels[0])
+print(training_images[0])
+
+
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+
+
+model = tf.keras.models.Sequential( )
+model.add(tf.keras.layers.Flatten())
+model.add( tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer = tf.train.AdamOptimizer(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=10,callbacks=[callbacks])
+
+#get losses and accuracy on test data
+model.evaluate(test_images, test_labels)
+
+
+classificationsProbs = model.predict(test_images)   #the probability that this item is each of the 10 classes
+classes=model.predict_classes(test_images)
+print(classificationsProbs[0])
+print(classes[0])
+print(test_labels[0])
+image=test_images[0]
+image = np.expand_dims(image, axis=0)
+predictedClassID=model.predict_classes(image)
