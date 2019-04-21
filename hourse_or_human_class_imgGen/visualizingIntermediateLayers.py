68b8367a5fefc12e26f63ce7df9f7822be3b015e
Walid Ahmed <walidahmed@Walids-MacBook-Air.local>
2019-04-21 09:01:47 -0400

    human_vs_horses

---
+++ hourse_or_human_class_imgGen/visualizingIntermediateLayers.py
@@ -0,0 +1,81 @@
+"""### Visualizing Intermediate Representations
+
+To get a feel for what kind of features our convnet has learned, one fun thing to do is to visualize how an input gets
+ transformed as it goes through the convnet.
+
+Let's pick a random image from the training set, 
+and then generate a figure where each row is the output of a layer, 
+and each image in the row is a specific filter in that output feature map. 
+Rerun this cell to generate intermediate representations for a variety of training images.
+"""
+
+
+
+
+
+model = load_model("hourse_or_human.keras")
+
+
+import numpy as np
+import random
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+# Let's define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model after
+# the first.
+successive_outputs = [layer.output for layer in model.layers[1:]]
+#visualization_model = Model(img_input, successive_outputs)
+visualization_model = tf.keras.models.Model(inputs = model.input, outputs = successive_outputs)
+# Let's prepare a random input image from the training set.
+horse_img_files = [os.path.join(train_horse_dir, f) for f in train_horse_names]
+human_img_files = [os.path.join(train_human_dir, f) for f in train_human_names]
+img_path = random.choice(horse_img_files + human_img_files)
+print("Visualizing img {0}".format(img_path))
+img = load_img(img_path, target_size=(300, 300))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+# Rescale by 1/255
+x /= 255
+
+# Let's run our image through our network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so can have them as part of our plot
+layer_names = [layer.name for layer in model.layers]
+
+# Now let's display our representations
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+  if len(feature_map.shape) == 4:
+    # Just do this for the conv / maxpool layers, not the fully-connected layers
+    n_features = feature_map.shape[-1]  # number of features in feature map
+    # The feature map has shape (1, size, size, n_features)
+    size = feature_map.shape[1]
+    # We will tile our images in this matrix
+    display_grid = np.zeros((size, size * n_features))
+    for i in range(n_features):
+      # Postprocess the feature to make it visually palatable
+      x = feature_map[0, :, :, i]
+      x -= x.mean()
+      x /= x.std()
+      x *= 64
+      x += 128
+      x = np.clip(x, 0, 255).astype('uint8')
+      # We'll tile each filter into this big horizontal grid
+      display_grid[:, i * size : (i + 1) * size] = x
+    # Display the grid
+    scale = 20. / n_features
+    plt.figure(figsize=(scale * n_features, scale))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+    plt.show()
+
+
+'''As you can see we go from the raw pixels of the images to increasingly abstract and compact representations. The representations downstream start highlighting what the network pays attention to, and they show fewer and fewer features being "activated"; most are set to zero. This is called "sparsity." Representation sparsity is a key feature of deep learning.
+
+
+These representations carry increasingly less information about the original pixels of the image, but increasingly refined information about the class of the image. You can think of a convnet (or a deep network in general) as an information distillation pipeline.
+
+'''
