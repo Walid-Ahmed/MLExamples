b4eb6bbb8284e548a85209ec073c8798a022662f
Walid Ahmed <walidahmed@Walids-MacBook-Air.local>
2019-04-28 08:59:12 -0400

    adding cats vs dogs

--- fashionMNIST/fashionMNIST_CNN.py
+++ fashionMNIST/fashionMNIST_CNN.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 import tensorflow as tf
 print(tf.__version__)
 mnist = tf.keras.datasets.fashion_mnist
@@ -19,26 +20,4 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 model.fit(training_images, training_labels, epochs=5)
-=======
-import tensorflow as tf
-print(tf.__version__)
-mnist = tf.keras.datasets.fashion_mnist
-(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
-training_images=training_images.reshape(60000, 28, 28, 1)
-training_images=training_images / 255.0
-test_images = test_images.reshape(10000, 28, 28, 1)
-test_images=test_images/255.0
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2,2),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.summary()
-model.fit(training_images, training_labels, epochs=5)
->>>>>>> 08fc7255dac6678d4e96ddd43e34fac098937e7f
 test_loss = model.evaluate(test_images, test_labels)
